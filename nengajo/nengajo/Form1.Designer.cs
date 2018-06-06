868ee66b94402c6cac5088c7645c929b0a4eb458
okuyama-sin <39941994+okuyama-sin@users.noreply.github.com>
2018-06-06 12:08:43 +0900

    JusyorokuSQL
    My first Commit

---
+++ nengajo/nengajo/Form1.Designer.cs
@@ -0,0 +1,387 @@
+﻿using System;
+
+namespace nengajo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "住所録データベース";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "姓";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(55, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(99, 19);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(55, 70);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(99, 19);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "名";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "郵便番号";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(34, 122);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(35, 19);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(98, 122);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(56, 19);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(32, 144);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 12);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "住所１";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(34, 159);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(255, 19);
+            this.textBox5.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(32, 182);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 12);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "住所２";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(34, 197);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(255, 19);
+            this.textBox6.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(160, 73);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(25, 12);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "名２";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(191, 45);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 19);
+            this.textBox7.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(160, 48);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(25, 12);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "姓２";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(297, 73);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(25, 12);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "名３";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(191, 70);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 19);
+            this.textBox8.TabIndex = 15;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(434, 73);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(25, 12);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "名４";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(328, 70);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 19);
+            this.textBox9.TabIndex = 17;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(571, 73);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(25, 12);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "名５";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(465, 70);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 19);
+            this.textBox10.TabIndex = 19;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(75, 125);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(17, 12);
+            this.label12.TabIndex = 21;
+            this.label12.Text = "―";
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(602, 70);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(100, 19);
+            this.textBox11.TabIndex = 22;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(34, 289);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(668, 150);
+            this.dataGridView1.TabIndex = 23;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_select);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 260);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "検索";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(708, 357);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 25;
+            this.button2.Text = "追加";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(709, 415);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 26;
+            this.button3.Text = "削除";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(709, 386);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 25;
+            this.button4.Text = "更新";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(115, 260);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 24;
+            this.button5.Text = "クリア";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "住所録データベース";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ nengajo/nengajo/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace nengajo
+{
+    public partial class Form1 : Form
+    {
+        private bool[] hantei= new bool[11];
+        private string find, ins, upd, del;
+        private DataSet ds;
+        private int r;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Control[] ctext = new Control[11] { textBox1, textBox2, textBox7, textBox8, textBox9,
+                textBox10, textBox11, textBox3, textBox4, textBox5, textBox6 };
+            string[] record = new string[11] { "姓", "名", "姓２", "名２", "名３", "名４",
+                "名５", "郵便番号１", "郵便番号２", "住所１", "住所２" };
+            for (int i = 0; i < 11; i++)
+            {
+                
+                hantei[i] = String.IsNullOrEmpty(((TextBox)ctext[i]).Text);
+                if (hantei[i]) { }
+                else
+                {
+                    //クエリ用コマンド（where以下）作成
+                    find += "(" + record[i] + " like '%" + ((TextBox)ctext[i]).Text + "%') and";
+                }
+            }
+            if (find == "")
+            {
+                MessageBox.Show("値が入っていません", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            find = find.Remove(find.Length - 3, 3);
+
+            //SQLサーバーからのデータ格納用DataSetを作成
+            ds = new DataSet();
+
+            //App.configのconnectionStrings内情報を取得してSQLサーバーへ接続
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DB"].ConnectionString);
+            con.Open();
+
+            try
+            {
+                //クエリ用コマンド確定し、findの内容をクリア
+                string sqlstr = "SELECT * FROM jusyoroku where " + find;
+                find = "";
+
+                //SQLサーバーへクエリ送信し、結果をsdaへ格納
+                SqlDataAdapter sda = new SqlDataAdapter(sqlstr,con);
+
+                //結果の形式はそのままにして、jusyorokuテーブルの内容をdataGridView1へ反映
+                sda.FillSchema(ds, SchemaType.Source);
+                sda.Fill(ds,"jusyoroku");
+                dataGridView1.DataSource = ds.Tables["jusyoroku"];
+            }
+            catch
+            {
+                MessageBox.Show("SQLエラー", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error); 
+            }
+            finally
+            {
+                // データベースの接続終了
+                con.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Control[] ctext = new Control[11] { textBox1, textBox2, textBox7, textBox8, textBox9,
+                textBox10, textBox11, textBox3, textBox4, textBox5, textBox6 };
+            ins = "姓, 名, 姓２, 名２, 名３, 名４, 名５, 郵便番号１, 郵便番号２, 住所１, 住所２) values ('";
+
+            for (int i = 0; i < 11; i++)
+            {
+                hantei[i] = String.IsNullOrEmpty(((TextBox)ctext[i]).Text);
+                if (hantei[i])
+                {
+                    ins += "','";
+                }
+                else
+                {
+                    ins += ((TextBox)ctext[i]).Text + "','";
+                }
+            }
+
+            ins = ins.Remove(ins.Length - 2, 2) + ")";
+
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DB"].ConnectionString);
+            con.Open();
+
+            try
+            {
+                string sqlstr = "insert into jusyoroku (" + ins;
+
+                ins = "";
+
+                SqlCommand sc = new SqlCommand(sqlstr, con);
+
+                sc.ExecuteNonQuery();
+                MessageBox.Show("レコードに追加しました。", "追加",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            catch
+            {
+                MessageBox.Show("SQLエラー", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                // データベースの接続終了
+                con.Close();
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            del = dataGridView1.Rows[r].Cells[0].Value.ToString();
+
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DB"].ConnectionString);
+            con.Open();
+
+            try
+            {
+                string sqlstr = "delete from jusyoroku where ID = " + del;
+
+                del = "";
+
+                SqlCommand sc = new SqlCommand(sqlstr, con);
+
+                sc.ExecuteNonQuery();
+                MessageBox.Show("レコードID" + dataGridView1.Rows[r].Cells[0].Value.ToString() + "を削除しました。", "更新",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            catch
+            {
+                MessageBox.Show("SQLエラー", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                // データベースの接続終了
+                con.Close();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            r = dataGridView1.CurrentCell.RowIndex;
+            Control[] ctext = new Control[11] { textBox1, textBox2, textBox7, textBox8, textBox9,
+                textBox10, textBox11, textBox3, textBox4, textBox5, textBox6 };
+            string[] record = new string[11] { "姓", "名", "姓２", "名２", "名３", "名４",
+                "名５", "郵便番号１", "郵便番号２", "住所１", "住所２" };
+            for (int i = 0; i < 11; i++)
+            {
+
+                hantei[i] = String.IsNullOrEmpty(((TextBox)ctext[i]).Text);
+                if (hantei[i]) { }
+                else
+                {
+                    upd += record[i] + " = '" + ((TextBox)ctext[i]).Text + "',";
+                    find += "(" + record[i] + " like '%" + ((TextBox)ctext[i]).Text + "%') and";
+                }
+            }
+            upd = upd.Remove(upd.Length - 1, 1);
+            find = find.Remove(find.Length - 3, 3);
+
+            ds = new DataSet();
+
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DB"].ConnectionString);
+            con.Open();
+
+            try
+            {
+                string sqlstr = "update jusyoroku set " + upd + " where ID = "
+                    + dataGridView1.Rows[r].Cells[0].Value.ToString();
+
+                upd = "";
+
+                SqlCommand sc = new SqlCommand(sqlstr, con);
+
+                sc.ExecuteNonQuery();
+                MessageBox.Show("レコードID" + dataGridView1.Rows[r].Cells[0].Value.ToString() + "を更新しました。", "更新",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                sqlstr = "SELECT * FROM jusyoroku where " + find;
+
+                find = "";
+
+                SqlDataAdapter sda = new SqlDataAdapter(sqlstr, con);
+
+                sda.FillSchema(ds, SchemaType.Source);
+                sda.Fill(ds, "jusyoroku");
+                dataGridView1.DataSource = ds.Tables["jusyoroku"];
+            }
+            catch
+            {
+                MessageBox.Show("SQLエラー", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                // データベースの接続終了
+                con.Close();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Control[] ctext = new Control[11] { textBox1, textBox2, textBox7, textBox8, textBox9,
+                textBox10, textBox11, textBox3, textBox4, textBox5, textBox6 };
+            for (int i = 0; i < 11; i++)
+            {
+                ctext[i].Text = "";
+            }
+        }
+
+        private void dataGridView1_select(object sender, EventArgs e)
+        {
+            r = dataGridView1.CurrentCell.RowIndex;            
+            textBox1.Text = dataGridView1.Rows[r].Cells[1].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[r].Cells[2].Value.ToString();
+            textBox7.Text = dataGridView1.Rows[r].Cells[3].Value.ToString();
+            textBox8.Text = dataGridView1.Rows[r].Cells[4].Value.ToString();
+            textBox9.Text = dataGridView1.Rows[r].Cells[5].Value.ToString();
+            textBox10.Text = dataGridView1.Rows[r].Cells[6].Value.ToString();
+            textBox11.Text = dataGridView1.Rows[r].Cells[7].Value.ToString();
+            textBox3.Text = dataGridView1.Rows[r].Cells[8].Value.ToString();
+            textBox4.Text = dataGridView1.Rows[r].Cells[9].Value.ToString();
+            textBox5.Text = dataGridView1.Rows[r].Cells[10].Value.ToString();
+            textBox6.Text = dataGridView1.Rows[r].Cells[11].Value.ToString();
+        }
+    }
+}
